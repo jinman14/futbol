89bbb299d09fce453eefc6b28a8c1d7b152e4013
Sebastian McKee <sebmcke@gmail.com>
2025-01-13 15:12:06 -0600

    Add gamesCSV method

--- lib/csv_helper.rb
+++ lib/csv_helper.rb
@@ -1,6 +1,8 @@
 require 'csv'
+require 'pry'
 class CSVHelper
     @@teams = []
+    @@games = []
 
     def self.teamsCSV(filepath)
         CSV.foreach(filepath, headers: true, header_converters: :symbol) do |row|
@@ -14,6 +16,24 @@ class CSVHelper
             )
         end
         @@teams
+    end
+
+    def self.gamesCSV(filepath)
+        CSV.foreach(filepath, headers: true, header_converters: :symbol) do |row|
+            @@games << Game.new(
+                row[:game_id].to_i,
+                row[:season].to_i,
+                row[:type],
+                row[:date_time],
+                row[:away_team_id].to_i,
+                row[:home_team_id].to_i,
+                row[:away_goals].to_i,
+                row[:home_goals].to_i,
+                row[:venue],
+                row[:venue_link]
+            )
+        end
+        @@games
     end
 
     def self.all_teams
--- spec/csv_helper_spec.rb
+++ spec/csv_helper_spec.rb
@@ -6,7 +6,7 @@ require './lib/team'
 SimpleCov.start
 
 describe CSVHelper do
-    describe '#teamsCSV' do
+    describe '#self.teamsCSV' do
         it 'can parse teams.csv' do
             teams = CSVHelper.teamsCSV('./data/teams.csv')
 
@@ -19,7 +19,24 @@ describe CSVHelper do
         end
     end
 
-    describe '#all_teams' do
+    describe '#self.gamesCSV' do
+        it 'can parse games.csv' do
+            games = CSVHelper.gamesCSV('./data/games.csv')
+
+            expect(games[0].game_id).to eq 2012030221
+            expect(games[0].season).to eq 20122013
+            expect(games[0].type).to eq "Postseason"
+            expect(games[0].date_time).to eq "5/16/13"
+            expect(games[0].away_team_id).to eq 3
+            expect(games[0].home_team_id).to eq 6
+            expect(games[0].away_goals).to eq 2
+            expect(games[0].home_goals).to eq 3
+            expect(games[0].venue).to eq "Toyota Stadium"
+            expect(games[0].venue_link).to eq "/api/v1/venues/null"
+        end
+    end
+
+    describe '#self.all_teams' do
         it 'can return an array of all teams' do
             expect(CSVHelper.all_teams.count).to eq(32)
         end
