4d7ea2f61d721ca59a0beeb42106e33ce236f59e
Sebastian McKee <sebmcke@gmail.com>
2025-01-14 14:49:26 -0600

    Remove unneeded sort

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -2,13 +2,7 @@ class GameStatistics
     @@games = CSVHelper.gamesCSV('./data/games.csv')
 
     def self.highest_total_score
-        sorted = sort_by_score
-        (sorted[-1].home_goals + sorted[-1].away_goals)
-    end
-
-    private
-
-    def self.sort_by_score
-        @@games.sort_by { |game| game.home_goals + game.away_goals }
+        highest_score = @@games.max_by { |game| game.home_goals + game.away_goals }
+        highest_score.home_goals + highest_score.away_goals
     end
 end
