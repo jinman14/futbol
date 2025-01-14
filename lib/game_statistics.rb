e405aeee08043aaeb929ae4802f9801157fac400
Sebastian McKee <sebmcke@gmail.com>
2025-01-14 14:47:01 -0600

    Add highest_total_score method

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -1,5 +1,14 @@
 class GameStatistics
     @@games = CSVHelper.gamesCSV('./data/games.csv')
 
-    
+    def self.highest_total_score
+        sorted = sort_by_score
+        (sorted[-1].home_goals + sorted[-1].away_goals)
+    end
+
+    private
+
+    def self.sort_by_score
+        @@games.sort_by { |game| game.home_goals + game.away_goals }
+    end
 end
