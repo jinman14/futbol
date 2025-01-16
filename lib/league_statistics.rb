e5ae092b351a60ed12b92a005effd20cfcb38a01
Joseph Bloom <jsphbloom@gmail.com>
2025-01-16 11:47:56 -0700

    thing

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -15,11 +15,11 @@ class League_Statistics
   end
 
   def best_offense
-    
+
   end
 
   def games_played(team_id)
-    @game_teams.count do |game_team|
+    @game_teams.find_all do |game_team|
       game_team.team_id == team_id
     end
   end
